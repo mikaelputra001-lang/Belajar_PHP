409006c33d64a32a7d10b867e3527d87f538d3f1
EL WAS HERE <mikael.putra001@gmail.com>
2026-09-12 21:00:23 +0700

    what i learn: 1. how to modify string (not all covered) 2. how to use string function

--- PHP/index.php
+++ PHP/index.php
@@ -51,3 +51,83 @@ print "but its return the value of 1 <br>";
 //When using single quotes, variables have to be inserted using the . operator, like this:
 print 'I Love Learning ' .$text. '<br>';
 
+$bool = false;
+$float = 3.14;
+$angka = -676767;
+$fruits = ["apple", "grape", "kiwi"];
+//use var_dump() to get data type
+var_dump($x);
+var_dump($text);
+var_dump($TEXT);
+var_dump($bool);
+var_dump($angka);
+var_dump($float);
+echo "<br>";
+var_dump($fruits);
+echo "<br>";
+class Car {
+    public $color;
+    public $model;
+    public function __construct($color, $model) {
+        $this->color = $color;
+        $this->model = $model;
+    }
+    public function message() {
+        return "My car is a " . $this->color . " " . $this->model . "!";
+    }
+}
+//this is object data type
+$myCar = new Car("red", "Volvo");
+var_dump($myCar);
+echo "<br>";
+$myHuzz = null;
+print "how many huzz did i have? : ";
+var_dump($myHuzz); echo '<br>';
+//changing data type
+$myData = 5;
+var_dump($myData); echo '<br>';
+$myData = "hello";
+var_dump($myData); echo '<br>';
+
+$number7 = 7;
+var_dump($number7); echo '<br>';
+$number7 = (string) $number7;
+var_dump($number7); echo '<br>';
+
+//single and double quotes different in string data type
+$name = "Mikael";
+print "my name is $name"; echo '<br>';
+print 'my name is $name'; echo '<br>';
+
+//string function
+//how to return the string length
+echo strlen("hello"); echo '<br>';
+//how to counts the number of words in a string
+echo str_word_count("hello world"); echo '<br>';
+// how to checks if a string contains a specific substring
+//str_contains() function is only available in PHP 8.0 and later. For older versions, use the strpos()
+$statement = "i love you 3000";
+var_dump(str_contains($statement, "love")); echo '<br>';
+var_dump(str_contains($statement, "Love")); echo '<br>';
+//how to searches for a specific text within a string
+echo strpos("Hello world!", "world"); echo '<br>';
+//how to checks if a string starts with a specific substring
+var_dump(str_starts_with($statement, "i love")); echo '<br>';
+//hoe to checks if a string ends with a specific substring
+var_dump(str_ends_with($statement, "3000")); echo '<br>';
+
+//modify string
+//how to return a string to uppercase
+echo strtoupper($name); echo '<br>';
+//how to return a string to lowercase
+echo strtolower($name); echo '<br>';
+//how to checks if a string ends with a specific substring
+echo str_replace("love", "hate", $statement); echo '<br>';
+//how to reverse a string
+echo strrev($statement); echo '<br>';
+//how to remove whitespace from a string
+$someText = "       Hello Worlds       !";
+echo trim($someText); echo '<br>';
+//how to split a string into an array
+$arrayStatement = explode(" ", $statement);
+print_r ($arrayStatement); echo '<br>';

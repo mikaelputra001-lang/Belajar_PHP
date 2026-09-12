431d3d1182c6347b674b1ac8a540f80f63916a65
EL WAS HERE <mikael.putra001@gmail.com>
2026-09-12 19:31:30 +0700

    what i learn: 1. how variable have different scope 2. how to out put a string with echo and print 3. how to use variable in string

--- PHP/index.php
+++ PHP/index.php
@@ -1,2 +1,53 @@
 <?php
-echo "hello world";
+echo "hello world <br>";
+
+//this is one line comment
+/*this is
+multiple line comment*/
+
+//this var scope is global but can't use inside function
+$x = 5;
+echo "$x from global variable<br>";
+function variable(): void
+{
+    //this var scope is local and can't use outside function
+    $x = 6;
+    echo "$x from local variable<br>";
+}
+
+function statics(): void
+{
+    //this var scope is static
+    static $y = 10;
+    echo "$y from static variable<br>";
+    $y++;
+}
+
+function globals(): void
+{
+    //to call global var inside function
+    global $x;
+    echo "$x from calling the global variable<br>";
+}
+variable();
+statics();
+statics();
+statics();
+globals();
+
+echo ("this is echo too! <br>");
+echo "<h2>this echo use html tag</h2>";
+echo "this ","string ","was ","made ","with ","multiple parameters<br>";
+
+$text = "PHP";
+$TEXT = "Banana";
+echo "I Love $text<br>";
+echo "I Love $TEXT<br>";
+
+print ("this is print <br>");
+print "<h2>its just do the same thing as echo</h2><br>";
+print "but its return the value of 1 <br>";
+
+//When using single quotes, variables have to be inserted using the . operator, like this:
+print 'I Love Learning ' .$text. '<br>';
+
